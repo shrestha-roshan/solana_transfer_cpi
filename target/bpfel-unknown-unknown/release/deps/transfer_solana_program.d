4095df07387c463889089671b047ccfc4b1707d5
roshans <shrestharoshan768@gmail.com>
2022-06-01 18:24:31 +0545

    deployed program

--- target/bpfel-unknown-unknown/release/deps/transfer_solana_program.d
+++ target/bpfel-unknown-unknown/release/deps/transfer_solana_program.d
@@ -1,8 +1,8 @@
-/home/rosh/solana_transfer/transfer-solana-program/target/bpfel-unknown-unknown/release/deps/transfer_solana_program.so: src/lib.rs src/entrypoint.rs src/instruction.rs src/processor.rs src/state.rs
+/home/rosh/solana_transfer_cpi/target/bpfel-unknown-unknown/release/deps/transfer_solana_program.so: src/lib.rs src/entrypoint.rs src/instruction.rs src/processor.rs src/state.rs
 
-/home/rosh/solana_transfer/transfer-solana-program/target/bpfel-unknown-unknown/release/deps/libtransfer_solana_program.rlib: src/lib.rs src/entrypoint.rs src/instruction.rs src/processor.rs src/state.rs
+/home/rosh/solana_transfer_cpi/target/bpfel-unknown-unknown/release/deps/libtransfer_solana_program.rlib: src/lib.rs src/entrypoint.rs src/instruction.rs src/processor.rs src/state.rs
 
-/home/rosh/solana_transfer/transfer-solana-program/target/bpfel-unknown-unknown/release/deps/transfer_solana_program.d: src/lib.rs src/entrypoint.rs src/instruction.rs src/processor.rs src/state.rs
+/home/rosh/solana_transfer_cpi/target/bpfel-unknown-unknown/release/deps/transfer_solana_program.d: src/lib.rs src/entrypoint.rs src/instruction.rs src/processor.rs src/state.rs
 
 src/lib.rs:
 src/entrypoint.rs:
